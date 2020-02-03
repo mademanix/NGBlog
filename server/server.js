d23f4f0e607172e052844c5b8ad428c7e8d30e11
ludwik siarzewsky <pradley1994@gmail.com>
2020-02-03 21:17:20 +0100

    this is a next some awesome stuff for node.js and mysql

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 const mysql = require('mysql');
+const express = require('express');
+const port = 3000;
 
-const mysqlConnection = mysql.createConnection({
-    host: 'foobar',
-    user: 'foobar',
-    password: 'foobar',
-    database: 'foobar'
-});
+const app = express();
 
-mysqlConnection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
+
+app.get('/', (req, res) => res.send('dupa'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// const mysqlConnection = mysql.createConnection({
+//     host: 'foobar',
+//     user: 'foobar',
+//     password: 'foobar',
+//     database: 'foobar'
+// });
+
+// mysqlConnection.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+// });
